c51562a4135bf8bd888f571d405570dacccb9795
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-11-15 14:39:45 +0100

    [fix] stesso nome

--- people.js
+++ people.js
@@ -4,7 +4,7 @@ const { addHobbies } = require('./hobbies.js');
 
 function combinePersonHobbies() {
     const result = makePerson('damiano', 'maugeri')
-    result.hoobbies = addHobbies('calcio', 'tennis', 'basket').hobbies
+    result.hobbies = addHobbies('calcio', 'tennis', 'basket').hobbies
 
     return result
 
